1280e6d730d16d0aea12fdeb777419f40314bd97
scbro <scbro@10.220.52.224>
2024-10-10 16:16:20 -0400

    Code for PA2 both a and b - Shaun Brothers

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,61 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import java.util.Scanner;
+
+/*
+ * This is Programming Assignment 02 Numbers
+ * By Shaun Brothers
+ * For Computer Science 1000-19K
+ * 
+ */
 public class PA2a {
 
-	// TODO: document this function
+	// This calculates the sum and averages of all the positive numbers, negative numbers, total numbers
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner scanner = new Scanner(System.in);
+
+        int pSum = 0;
+        int npSum = 0;
+        int tSum = 0;
+        int pCount = 0;
+        int npCount = 0;
+
+        System.out.printf("Enter five whole numbers: ");
+
+        for (int i = 0; i < 5; i++) {
+            int number = scanner.nextInt();
+
+            tSum += number;
+
+            if (number > 0) {
+                pSum += number;
+                pCount++;
+            } else {
+                npSum += number;
+                npCount++;
+            }
+        }
+
+        scanner.close();
+
+        double pAvg = 0.0;
+        if (pCount > 0) {
+            pAvg = (double) pSum / pCount;
+        }
+
+        double npAvg = 0.0;
+        if (npCount > 0) {
+            npAvg = (double) npSum / npCount;
+        }
+
+        double totalAvg = (double) tSum / 5;
+
+        System.out.println("The sum of the " + pCount + " positive numbers: " + pSum);
+        System.out.println("The sum of the " + npCount + " non-positive numbers: " + npSum);
+        System.out.println("The sum of the 5 numbers: " + tSum);
+        System.out.println("The average of the " + pCount + " positive numbers: " + String.format("%.2f", pAvg));
+        System.out.println("The average of the " + npCount + " non-positive numbers: " + String.format("%.2f", npAvg));
+        System.out.println("The average of the 5 numbers: " + String.format("%.2f", totalAvg));
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,47 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import java.util.Scanner;
+
+/*
+ * This is Programming Assignment 02 Numbers
+ * By Shaun Brothers
+ * For Computer Science 1000-19K
+ * 
+ */
 public class PA2b {
 
-	// TODO: document this function
+	// This code solves the quadratic equation whilst telling the roots and if there are no roots it will say imaginary
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a b c: ");
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        double total = b * b - 4 * a * c;
+
+        if (total < 0) {
+            System.out.println("Roots: imaginary");
+        } else if (total == 0) {
+            double root = -b / (2 * a);
+            System.out.printf("Root: %.2f%n", root);
+        } else {
+            double root1 = (-b - Math.sqrt(total)) / (2 * a);
+            double root2 = (-b + Math.sqrt(total)) / (2 * a);
+
+           
+            if (root1 > root2) {
+                double temp = root1;
+                root1 = root2;
+                root2 = temp;
+            }
+
+            System.out.printf("Roots: %.2f, %.2f%n", root1, root2);
+        }
+
+        scanner.close();
+
 	}
 
 }
